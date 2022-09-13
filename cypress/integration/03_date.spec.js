4cd5326ccd517a03ec9c93703df690c605a45abb
Muhammad Bilal <muhammadbilal@nxb.com.pk>
2022-09-13 19:09:37 +0500

    Add base URL

--- cypress/integration/03_date.spec.js
+++ cypress/integration/03_date.spec.js
@@ -1,13 +1,13 @@
 /// <reference types="Cypress" />
 
-context('Date', () => {
-  it('Displays the correct date info', () => {
+context("Date", () => {
+  it("Displays the correct date info", () => {
     cy.clock(new Date(104907600000));
-    cy.visit(Cypress.env('baseUrl'));
+    cy.visit("http://localhost:3000");
 
-    cy.getByTestId('day').should('have.text', '29');
-    cy.getByTestId('year').should('have.text', '1973');
-    cy.getByTestId('month').should('have.text', 'Apr');
-    cy.getByTestId('week-day').should('have.text', 'Sunday');
+    cy.getByTestId("day").should("have.text", "29");
+    cy.getByTestId("year").should("have.text", "1973");
+    cy.getByTestId("month").should("have.text", "Apr");
+    cy.getByTestId("week-day").should("have.text", "Sunday");
   });
 });
